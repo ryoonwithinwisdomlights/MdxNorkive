c4be07cbb5ba3c428f69220ef6139bc8768e2da2
ryoon-with-wisdomtrees <ryoon.with.wisdomtrees@gmail.com>
2024-12-08 00:59:04 +0900

    fix: loading.tsx 변경

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,28 @@
-import PageLoader from "@/components/ui/page-loader";
+"use client";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+
+// 사전에 사용할 아이콘 추가
+library.add(faSpinner);
 
 const Loading = () => {
   return (
     <div className=" justify-center items-center w-full h-full">
-      <PageLoader />
+      <div
+        id="cover-loading"
+        className={
+          "z-50 opacity-50pointer-events-none transition-all duration-300"
+        }
+      >
+        <div className="w-full h-screen flex justify-center items-center">
+          Loading...
+          <FontAwesomeIcon
+            className="text-3xl text-black dark:text-white animate-spin"
+            icon={faSpinner}
+          />
+        </div>
+      </div>
     </div>
   );
 };
