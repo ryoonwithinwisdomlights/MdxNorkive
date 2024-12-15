0ed6220d54d4c59c4d0913d6966154c014b010e1
ryoon-with-wisdomtrees <ryoon.with.wisdomtrees@gmail.com>
2024-12-15 15:27:28 +0900

    fix:package.json

--- lib/providers/ModalProvider.tsx
+++ lib/providers/ModalProvider.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import ThemeSettingModal from "@/components/modals/SettingModal";
 import { Toaster } from "sonner";
 
 export const ModalProvider = () => {
