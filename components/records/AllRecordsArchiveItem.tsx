499cd39ab2183f77d56519f1f961ae156c64f805
ryoon-with-wisdomtrees <ryoon.with.wisdomtrees@gmail.com>
2024-12-18 21:28:03 +0900

    fix:메인 레이아웃 레코드 ui 일부분 수정

--- components/records/AllRecordsArchiveItem.tsx
+++ components/records/AllRecordsArchiveItem.tsx
@@ -31,34 +31,36 @@ export default function AllRecordsArchiveItem({
         {archiveRecords[archiveTitle]?.map((post) => (
           <li
             key={post.id}
-            className="border-l-4 border-[#f1efe9e2]  hover:border-[#cbcac4e2] flex flex-row p-2 text-xs md:text-base items-center text-justify  hover:scale-x-105  dark:hover:border-neutral-400 dark:border-neutral-400/30 transform duration-500"
+            className="border-l-4 border-[#f1efe9e2]  hover:border-[#cbcac4e2] 
+             p-2 text-xs md:text-base 
+             text-justify  hover:scale-x-105  dark:hover:border-neutral-400 dark:border-neutral-400/30 transform duration-500"
           >
-            <div id={post?.publishDay}>
-              <span className="text-[#cbcac4e2] dark:hover:text-neutral-300">
-                {post.date?.start_date}
-              </span>{" "}
-              <span
-                key={post.id}
-                className="pl-2 pr-3 text-xs text-justify lh "
-              >
-                {post.pageIcon}
-              </span>
+            <div
+              id={post?.publishDay}
+              className="flex flex-row items-start justify-start  "
+            >
               <Link
                 href={`records/${post.id}`}
-                className="dark:text-neutral-400 hover:text-neutral-400  dark:hover:text-neutral-200 overflow-x-hidden hover:underline cursor-pointer text-neutral-600"
+                className="dark:text-neutral-400
+                 hover:text-neutral-400  dark:hover:text-neutral-200 overflow-x-hidden 
+                 hover:underline cursor-pointer text-neutral-600"
               >
-                {post.title}{" "}
-                <span className="text-xs">
-                  {/* &nbsp;{post.password !== '' && '🔐'} */}
-                  &nbsp;
-                  {post.password !== "" && (
-                    <>
-                      <LockIcon className="mr-1" />
-                      &nbsp;비공개
-                    </>
-                  )}
+                <span className="text-[#cbcac4e2] dark:hover:text-neutral-300 ">
+                  {post.date?.start_date}
                 </span>
+
+                <span className="pl-2 pr-3 text-xs  ">{post.pageIcon}</span>
+                <span className=" ">{post.title}</span>
               </Link>
+              <span className="pl-2 pr-3  flex flex-row justify-start items-center text-neutral-500">
+                &nbsp;
+                {post.password !== "" && (
+                  <>
+                    <LockIcon className="mr-1 w-3" />
+                    비공개
+                  </>
+                )}
+              </span>
             </div>
           </li>
         ))}
