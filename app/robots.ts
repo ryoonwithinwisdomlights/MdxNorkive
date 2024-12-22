a0daa6b607c5098eb5141200e94cf89813d26c00
ryoon-with-wisdomtrees <ryoon.with.wisdomtrees@gmail.com>
2024-12-23 02:01:59 +0900

    fix:robot.ts 변경

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,16 @@
 import type { MetadataRoute } from "next";
 import { BLOG } from "@/blog.config";
-
+/**
+ * 
+ * robots.txt 파일의 구성은 다음과 같습니다.
+    User-Agent: 크롤러의 이름을 나타내는 요소입니다. "*"는 모든 크롤러를 의미합니다.
+    Allow: 크롤러가 접근할 수 있는 URL을 나타내는 요소입니다.
+    Disallow: 크롤러가 접근할 수 없는 URL을 나타내는 요소입니다.
+    Sitemap: 사이트맵 파일의 URL을 나타내는 요소입니다. 이 요소는 사이트맵 파일의 URL을 검색 엔진에게 알려줍니다.
+ * 
+ * 
+ * 
+ */
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
@@ -9,6 +19,6 @@ export default function robots(): MetadataRoute.Robots {
       disallow: "/private/",
     },
     host: BLOG.LINK,
-    // sitemap: `${BLOG.LINK}/sitemap.xml`,
+    sitemap: `${BLOG.LINK}sitemap.xml`,
   };
 }
