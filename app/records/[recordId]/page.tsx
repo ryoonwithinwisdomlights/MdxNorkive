3d9048edea31d6746718d1aed2f1d35b4a8326d9
ryoon-with-wisdomtrees <ryoon.with.wisdomtrees@gmail.com>
2024-12-25 18:51:38 +0900

    fix: adopt Incremental Static Regeneration

--- app/records/[recordId]/page.tsx
+++ app/records/[recordId]/page.tsx
@@ -1,18 +1,34 @@
+import { BLOG } from "@/blog.config";
 import SingleRecords from "@/components/records/SingleRecords";
 import { generatingPageByTypeAndId } from "@/lib/notion/getNotionData";
 
+// Next.js will invalidate the cache when a
+// request comes in, at most once every {BLOG.REVALIDATE} seconds.
+export const revalidate = 60;
+
+// We'll prerender only the params from `generateStaticParams` at build time.
+// If a request comes in for a path that hasn't been generated,
+// Next.js will server-render the page on-demand.
+//see the details https://nextjs.org/docs/app/building-your-application/data-fetching/incremental-static-regeneration
+export const dynamicParams = true; // or false, to 404 on unknown paths
+
 export async function generateStaticParams() {
-  const records = [
-    { recordId: "1481eb5c-0337-8087-a304-f2af3275be11" },
-    { recordId: "another-record-id" },
-  ];
+  const records: any =
+    BLOG.LINK !== ""
+      ? await fetch("https://api.vercel.app/blog").then((res) => res.json())
+      : [{ recordId: "1481eb5c-0337-8087-a304-f2af3275be11" }];
+
   return records.map((record) => ({
     recordId: record.recordId,
   }));
 }
 
 // `generateStaticParams`가 반환한 `params`를 사용하여 이 페이지의 여러 버전이 정적으로 생성됩니다.
-export default async function Page({ params }) {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ recordId: string }>;
+}) {
   const { recordId } = await params;
 
   const props = await generatingPageByTypeAndId(recordId, "Record");
