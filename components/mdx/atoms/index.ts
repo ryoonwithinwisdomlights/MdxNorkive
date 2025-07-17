c49044928f82b0d721949b39897465950917b431
ryoon-with-wisdomtrees <ryoon.with.wisdomtrees@gmail.com>
2025-07-17 17:37:28 +0900

    refactor:components/mdx/atoms/index.ts 파일을 생성해 각 아톰 컴포넌트를 모듈화하고 export 구조를 정리.MDXContent.tsx 혹은 parseMDX.ts 등에서 import * as Atoms from @/components/mdx/atoms;처럼 사용가능하게 적용

---
+++ components/mdx/atoms/index.ts
@@ -0,0 +1,11 @@
+// components/mdx/atoms/index.ts
+
+export { Paragraph } from "./Paragraph";
+export { Heading } from "./heading";
+export { Blockquote } from "./blockquote";
+export { UnorderedList } from "./UnorderedList";
+export { OrderedList } from "./OrderedList";
+export { ListItem } from "./ListItem";
+export { InlineCode } from "./InlineCode";
+export { Strong } from "./Strong";
+export { HR } from "./HorizontalRule";
