d7b2bcd7a9bb6e93cc9b0e2ade71de48b943d2c3
ryoon-with-wisdomtrees <ryoon.with.wisdomtrees@gmail.com>
2025-08-07 18:52:18 +0900

    fix: 일부 ui 수정

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <GeneralRecordTypePageWrapper>
       {/* <LeftSidebar /> */}
       <div
-        className="w-full mt-[20px] p-10 xl:w-[60%] flex flex-col  pb-20 md:p-0
+        className="w-full mt-[20px] p-10 xl:w-[60%] flex flex-col  pb-20 
       justify-center items-center "
       >
         {children}
--- types/layout.props.ts
+++ types/layout.props.ts
@@ -1,2 +1,2 @@
 export const basicDocsClass =
-  "xl:w-[calc(100vw-600px)] mt-[20px] w-full  flex flex-col justify-center items-center  pb-20 md:p-0  ";
+  "xl:w-[calc(100vw-600px)] mt-[20px] w-full  flex flex-col justify-center items-center  pb-20   ";
