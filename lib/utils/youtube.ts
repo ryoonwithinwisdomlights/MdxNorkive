87e9188e58f749fca465e7dd1f4d95cd8928c1b6
ryoon-with-wisdomtrees <ryoon.with.wisdomtrees@gmail.com>
2025-11-01 15:08:23 +0900

    feat:youtubeutils package제작

--- lib/utils/youtube.ts
+++ lib/utils/youtube.ts
@@ -1,2 +1,2 @@
-// Re-export from @norkive/youtube-utils package
-export { getYoutubeId, isValidYoutubeUrl, getYoutubeParams } from '@norkive/youtube-utils';
+// Re-export from norkive-youtube-utils package
+export { getYoutubeId, isValidYoutubeUrl, getYoutubeParams } from 'norkive-youtube-utils';
